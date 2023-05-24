4c742c20aee25be2d9111da1a04d2ef086225421
NikiforovSaveliy <113004532+NikiforovSaveliy@users.noreply.github.com>
2023-05-24 23:35:57 +0300

    add comments

--- 1.py
+++ 1.py
@@ -2,29 +2,26 @@ game_filed = [[None] * 3] * 3
 
 
 def input_game_field():
+    '''Функция для ввода игрового поля'''
     return [input().split(sep=' ') for i in range(3)]
 
 
 def print_game_field(game_filed):
+    '''Функяи для вывод игрового поля'''
     for i in game_filed:
         print(i)
 
 
 def get_elements(a: list, indexes: tuple[int, int]):
+    """Функция, которая возвращает элемент двумерного массива"""
     i, j = indexes
     return a[i][j]
 
 
 def check_victory(game_field, player_a, player_b):
+    """Фукнция проверки победы"""
+
     # Все выйгрышные позиции
-    '''
-           [(0, 0), (0, 1), (0, 2)],
-           [(1, 0), (1, 1), (1, 2)],
-           [(2, 0), (2, 1), (2, 2)],
-           [(0, 0), (1, 0), (2, 0)],
-           [(0, 1), (1, 1), (2, 1)],
-           [(0, 2), (1, 2), (2, 2)],
-           '''
     vin_pos = [
         [(0, 0), (0, 1), (0, 2)],
         [(1, 0), (1, 1), (1, 2)],
@@ -35,12 +32,15 @@ def check_victory(game_field, player_a, player_b):
         [(0, 0), (1, 1), (2, 2)],
         [(0, 2), (1, 1), (2, 0)]
     ]
+    #Проверяем кажду выигрышную последовательность
     for i in vin_pos:
+        #Если у кого-то выигрышная стратегия, то просто возвращаем победителя
         if all([get_elements(game_field, j) == player_a for j in i]):
             return f'player {player_a} won'
         if all([get_elements(game_field, j) == player_b for j in i]):
             return f'player {player_b} won'
 
+    # Если нет выигрышной позиции ни у кого, то присваивается ничья...
     return 'DRAW!!!'
 
 
