b892566e63c3db3dc89e1a703f6c2d78bdc0ccff
NikiforovSaveliy <113004532+NikiforovSaveliy@users.noreply.github.com>
2023-05-24 23:33:17 +0300

    first task

--- 1.py
+++ 1.py
@@ -1,48 +1,51 @@
+game_filed = [[None] * 3] * 3
 
-def find(A, weights, k ,s, result):
-    if A[k][s] == 0:
-        return result
-    if A[k-1][s] == A[k][s]:
-        find(A, weights, k-1, s, result)
-    else:
-        result.append(k-1)
-        find(A, weights, k-1, s-weights[k-1], result)
-        
-def make_matrix(jews: list , max_weight):
-    weights, prices = list(zip(*jews))
-    count = len(weights)
-    A = [ [0 for i in range(max_weight+1)] for i in range(count+1)]
-    for k in range(0, count+1): # Проход по предметам
-        for s in range(0, max_weight+1): # Проход по весам 
-            if k == 0 or s == 0: 
-                A[k][s] = 0 # Нули, потому что во вместимость нуль ничего не влезет :)
-            else:
-                if s >= weights[k-1]:
-                    A[k][s] = max(A[k-1][s],A[k-1][s- weights[k-1]] + prices[k-1])
-                    #Выбор элементов, если берем или не берем определенный элемент 
-                else:
-                    A[k][s] = A[k-1][s]
-    result = []
-    find(A, weights, count, max_weight,result)
-    _ = [jews[i] for i in result]
-    
-    for i in _: jews.remove(i)
-    
-    return _
 
-def find_result(jews, max_weight, repeats):
-    result = []
-    for i in range(repeats):
-        result += make_matrix(jews, max_weight)
-    return result, sum([i[1] for i in result])
-             
-weights = [3,4,5,8,9]
-prices = [1,6,4,7,6]
-jews = list(zip(weights, prices))
+def input_game_field():
+    return [input().split(sep=' ') for i in range(3)]
 
-print(jews)
-print('*' *80)
-print('Использованные:', find_result(jews, 13, 2)[0])
-print('*' *80)
-print('Оставшиеся:',jews)
 
+def print_game_field(game_filed):
+    for i in game_filed:
+        print(i)
+
+
+def get_elements(a: list, indexes: tuple[int, int]):
+    i, j = indexes
+    return a[i][j]
+
+
+def check_victory(game_field, player_a, player_b):
+    # Все выйгрышные позиции
+    '''
+           [(0, 0), (0, 1), (0, 2)],
+           [(1, 0), (1, 1), (1, 2)],
+           [(2, 0), (2, 1), (2, 2)],
+           [(0, 0), (1, 0), (2, 0)],
+           [(0, 1), (1, 1), (2, 1)],
+           [(0, 2), (1, 2), (2, 2)],
+           '''
+    vin_pos = [
+        [(0, 0), (0, 1), (0, 2)],
+        [(1, 0), (1, 1), (1, 2)],
+        [(2, 0), (2, 1), (2, 2)],
+        [(0, 0), (1, 0), (2, 0)],
+        [(0, 1), (1, 1), (2, 1)],
+        [(0, 2), (1, 2), (2, 2)],
+        [(0, 0), (1, 1), (2, 2)],
+        [(0, 2), (1, 1), (2, 0)]
+    ]
+    for i in vin_pos:
+        if all([get_elements(game_field, j) == player_a for j in i]):
+            return f'player {player_a} won'
+        if all([get_elements(game_field, j) == player_b for j in i]):
+            return f'player {player_b} won'
+
+    return 'DRAW!!!'
+
+
+if __name__ == '__main__':
+    player_a = input('First player name: ')
+    player_b = input('Second player name: ')
+    game_filed = input_game_field()
+    print(check_victory(game_filed, player_a, player_b))
